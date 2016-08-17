3cf4852d4ab9e8508f455efbf530cee68464664a
Jack Averill <jfaverill@gmail.com>
2016-08-18 00:22:37 +0700

    Install Laravel

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Laravel</title>
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
